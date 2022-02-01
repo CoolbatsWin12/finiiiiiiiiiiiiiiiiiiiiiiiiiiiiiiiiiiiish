ec6325f6aabe82b158dda55997f7a65d8dd050da
CoolbatsWin12 <97554534+CoolbatsWin12@users.noreply.github.com>
2022-02-01 19:42:34 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,130 @@
+var score=0
+var gamestate= "play"
+function preload(){
+  rex=loadAnimation("trex1.png","trex3.png","trex4.png")
+  static=loadAnimation("trex_collided.png")
+  grounds=loadImage("ground2.png")
+  cloudexe=loadImage("cloud.png")
+  ob1=loadImage("obstacle1.png")
+  ob2=loadImage("obstacle2.png")
+  ob3=loadImage("obstacle3.png")
+  ob4=loadImage("obstacle4.png")
+  ob5=loadImage("obstacle5.png")
+  ob6=loadImage("obstacle6.png")
+  gm=loadImage("gameOver.png")
+  reset=loadImage("restart.png")
+  check=loadSound("checkPoint.mp3")
+  di=loadSound("die.mp3")
+  jump=loadSound("jump.mp3")
+}
+
+function setup(){
+  createCanvas(600,200)
+  trex=createSprite(60,180,10,5)
+  trex.addAnimation("sprint",rex)
+  trex.addAnimation("The end",static)
+  trex.scale=0.5
+  trex.debug=false
+  trex.setCollider("circle",0,0,45)
+  ground=createSprite(300,180,600,20)
+  ground.addImage(grounds)
+  ground2=createSprite(300,190,600,20)
+  ground2.visible=false
+  gameover=createSprite(300,100,10,15)
+  gameover.addImage(gm)
+  gameover.scale=0.5
+  restart=createSprite(300,130,10,15)
+  restart.addImage(reset)
+  restart.scale=0.5
+  gameover.visible=false
+  restart.visible=false
+  obg=createGroup()
+  cg=createGroup()
+}
+
+function draw(){
+  background(180)
+  textFont("Comic Sans MS")
+  textSize(20)
+  text("Score: "+score,450,20)
+  if(gamestate==="play"){
+  score=score+Math.round(getFrameRate()/60)
+  trex.collide(ground2)
+  if(keyDown("space") && trex.y>156){
+    trex.velocityY=-10
+    jump.play()
+  }
+  trex.velocityY=trex.velocityY+0.6
+  ground.velocityX=-(3+score/50)
+  if(ground.x<0){
+    ground.x=600
+  }
+  clouds()
+  obs()
+  if(score %100===0){
+    check.play()
+  }
+  if(trex.isTouching(obg)){
+    gamestate="end"
+    di.play()
+  }
+}
+
+if(gamestate==="end"){
+  ground.velocityX=0
+  trex.velocityY=0
+  obg.setVelocityXEach(0)
+  cg.setVelocityXEach(0)
+  obg.setLifetimeEach(-5)
+  cg.setLifetimeEach(-5)
+  trex.changeAnimation("The end",static)
+  gameover.visible=true
+  restart.visible=true
+  if(mousePressedOver(restart)){
+    gamestate="play"
+    obg.destroyEach()
+    cg.destroyEach()
+    score=0
+    gameover.visible=false
+  restart.visible=false
+  trex.changeAnimation("sprint",rex)
+  }
+}
+  drawSprites()
+}
+
+function clouds(){
+  if(frameCount %50===0){
+  cloud=createSprite(600,random(30,100),10,10)
+  cloud.velocityX = -4
+  cloud.addImage(cloudexe)
+  cloud.scale = 0.8
+  trex.depth=cloud.depth+1
+  cloud.lifetime=160
+  cg.add(cloud)
+  }
+}
+
+function obs(){
+  if(frameCount %60===0){
+  ob=createSprite(600,165,10,10)
+  ob.velocityX = -(4+score/50)
+  ob.scale=0.5
+  ob.lifetime=160
+  obg.add(ob)
+  switch(Math.round(random(1,6))){
+    case 1:ob.addImage(ob1)
+    break
+    case 2:ob.addImage(ob2)
+    break
+    case 3:ob.addImage(ob3)
+    break
+    case 4:ob.addImage(ob4)
+    break
+    case 5:ob.addImage(ob5)
+    break
+    case 6:ob.addImage(ob6)
+    break
+  }
+  }
+}
